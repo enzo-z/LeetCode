e0c197a1e0b6d83faa1fce5106826378e1bd315a
Enzo Zamora <enzo.roncoli@fadami.com.br>
2022-07-17 22:40:12 -0300

    Demonstrating the Euclidean Algorithm for the HCF between two numbers

---
+++ ComputerConcepts/Mathematics/GreatestCommonDivider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerConcepts.Mathematics
+{
+    /// <summary>
+    /// Euclidian Algorithm for finding the greatest common divider (GDC) or HCF (highest common factor), given two natural numbers (a and b)
+    /// </summary>
+    public class GreatestCommonDivider
+    {
+        // https://www.youtube.com/watch?v=yHwneN6zJmU&ab_channel=NesoAcademy
+        public static int FindGDC(int a, int b)
+        {
+            if (a < b)
+            {
+                int aux = a;
+                a = b;
+                b = aux;
+            }
+            return (b == 0) ? a : FindGDC(b, a % b);
+        }
+    }
+}
---
+++ ComputerConcepts/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ComputerConcepts.Mathematics;
+
+Console.WriteLine("Hello, World!");
+
+
+int x = GreatestCommonDivider.FindGDC(252, 105);
+
+Console.WriteLine(x);
