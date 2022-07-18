21632feb89bf3b2fe6e0426854033aebc72d85df
Enzo Zamora <enzo.roncoli@fadami.com.br>
2022-07-17 22:39:01 -0300

    Doing some challenges from a competition hosted by UFES (FEDERAL UNIVERSITY OF ESPIRITO SANTO) that my friend sent to me.

---
+++ LeetCode/TopCom/QuestaoA.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TopCom
+{
+    /// <summary>
+    /// <a href="https://drive.google.com/file/d/1QE4PiHmYFz-V0KtCh8di46VfZwrZIjSB/view?usp=sharing">Questão original (PT-BR)</a>
+    /// </summary>
+    public class QuestaoA
+    {
+        public static void Start()
+        {
+            try
+            {
+                int n = GetQntdValores();
+                if (n == 0)
+                {
+                    Console.WriteLine("false");
+                    return;
+                }
+
+                var mdcs = GetMaioresDivisoresComuns(n);
+                VerificaCoprimos(mdcs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                PrintErrorMsg();
+            }
+        }
+        private static void VerificaCoprimos(List<int> mdcs)
+        {
+            foreach (var mdc in mdcs)
+            {
+                if (mdc == 1)
+                    Console.WriteLine("true");
+                else
+                    Console.WriteLine("false");
+            }
+        }
+        public static List<int> GetMaioresDivisoresComuns(int n)
+        {
+            var mdcs = new List<int>();
+            var doisNum = new int[2];
+            for (int i = 0; i < n; i++)
+            {
+                string? inputValores = Console.ReadLine();
+                doisNum = ValidaInput(inputValores);
+                int maiorDivisorComum = GetMaiorDivisorComum(doisNum[0], doisNum[1]);
+                mdcs.Add(maiorDivisorComum);
+            }
+            return mdcs;
+        }
+        public static int GetMaiorDivisorComum(int a, int b)
+        {
+            if (a < b)
+            {
+                int aux = a;
+                a = b;
+                b = aux;
+            }
+
+            return (b == 0) ? a : GetMaiorDivisorComum(b, a % b);
+        }
+        public static int[] ValidaInput(string? valores)
+        {
+            if (valores is null) throw new ArgumentNullException(nameof(valores));
+
+            var arrValores = valores.Split(" ");
+
+            if (arrValores.Length < 2) throw new Exception("Precisa passar dois valores!");
+
+            int n1 = 0;
+            int n2 = 0;
+
+            n1 = Convert.ToInt32(arrValores[0]);
+            n2 = Convert.ToInt32(arrValores[1]);
+
+            return new int[] { n1, n2 };
+        }
+        public static int GetQntdValores()
+        {
+            Console.WriteLine("Digite n (qntd de linhas a serem lidas): ");
+            int n = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > 1000) throw new Exception("n deve ser <= a 1000");
+
+            return n;
+        }
+        public static void PrintErrorMsg() => Console.WriteLine("Feriu as regras! Invalido!");
+    }
+}
---
+++ LeetCode/TopCom/QuestaoC.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TopCom
+{
+
+    /// <summary>
+    /// <a href="https://drive.google.com/file/d/1sBcVs8a4nVmBU-A5Z093LJOQpBV5akcw/view?usp=sharing">Questão original (PT-BR)</a>
+    /// </summary>
+    public class QuestaoC
+    {
+        public static void Start()
+        {
+            try
+            {
+                int qntdInputs = GetQntdValores();
+                if (qntdInputs == 0) Console.WriteLine("Nenhum input");
+                for (int i = 0; i < qntdInputs; i++)
+                {
+                    var input = Console.ReadLine();
+                    string balanceamento = GetBalanceamento(input);
+                    Console.WriteLine(balanceamento);
+                }
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Você não digitou um número!");
+                Start();
+            }
+        }
+
+        public static int GetQntdValores()
+        {
+            Console.WriteLine("Digite o número inputs (n): ");
+            int n = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        public static string GetBalanceamento(string? input)
+        {
+            if (input == null) return "Nenhum parentesis. Balanceada";
+            
+            int balanceamento = 0;
+            var abertos = new Stack<char>();
+            foreach (var character in input.ToCharArray())
+            {
+                if (character == '(' || character == '[' || character == '{')
+                    abertos.Push(character);
+                else if (character == ')' || character == ']' || character == '}')
+                {
+                    if (balanceamento <= 0) return "Erro de balanceamento";
+                    
+                    else if (!IsAninhado(abertos.Pop(), character))
+                        return "Erro de balanceamento";
+                }
+                balanceamento = abertos.Count;
+            }
+            if (balanceamento != 0) return "Erro de balanceamento";
+
+            return "Balanceada";
+        }
+        public static bool IsAninhado(char lastOpen, char closing)
+        {
+            if(lastOpen == '(')
+                return closing == ')';
+            if (lastOpen == '{')
+                return closing == '}';
+            if (lastOpen == '[')
+                return closing == ']';
+            return false;
+        }
+
+    }
+}
